ac06279670913dfa0a0ae52c7c5827796b4ac965
Luke <33379089+lucas-j-k@users.noreply.github.com>
2018-01-14 17:30:56 +0000

    Added basic method to track current visual list of snippets in composer and store this as an array in the composer panel state, so we can keep track of the visual order as user drags and drops the paragraphs around

--- src/components/composer-panel.js
+++ src/components/composer-panel.js
@@ -5,7 +5,12 @@ class ComposerPanel extends Component {
 
   constructor(props) {
     super(props)
+    this.state = {
+      newOrder: []
+    }
+    //Bind custom methods to this
     this.deleteSnippet = this.deleteSnippet.bind(this);
+    this.updateComposerOrder = this.updateComposerOrder.bind(this);
   }
 
   //Handle when a user clicks delete icon inside a composer paragraph
@@ -14,14 +19,29 @@ class ComposerPanel extends Component {
     this.props.deleteSnippetFromComposer(index);
   }
 
+  //When a paragraph is re-ordered, we need to update the composer-contents State
+  updateComposerOrder(e){
+    let nodeList = Array.from(this.paraList.children[0].children);
+    let mappedArray = nodeList.map((node, index)=>{
+      return node.innerHTML;
+    });
+    console.log(mappedArray);
+    this.setState({
+      newOrder: mappedArray
+    });
+  }
+
   render(){
     //Map through the templates currently loaded into the composer array, and print them to the composer panel
     const composerContents = this.props.composerContents.map((snippet, index)=>{
-      return <p key={index}>{snippet.text}<a href="" onClick={(e) => this.deleteSnippet(index, e)}>[X] {index}</a></p>
+      return (<p key={index}>
+              {snippet.text}
+              <button href="" onClick={(e) => this.deleteSnippet(index, e)}>[X] {index}</button>
+              </p>)
     });
     return (
-      <div className="col-md-7" ref={this.dragulaDecorator}>
-        {composerContents}
+      <div className="col-md-7" onMouseLeave={(e) => this.updateComposerOrder(e)} ref={(paraList)=>{ this.paraList = paraList }}>
+        <div ref={this.dragulaDecorator}>{composerContents}</div>
       </div>
     )
   }

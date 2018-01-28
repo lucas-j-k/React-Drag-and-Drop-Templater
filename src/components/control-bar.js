6177418f9a18401b3fc7115e3f29c23d7e7dc86c
Luke <33379089+lucas-j-k@users.noreply.github.com>
2018-01-28 00:14:58 +0000

    Restyled the interface buttons

--- src/components/control-bar.js
+++ src/components/control-bar.js
@@ -24,13 +24,31 @@ class ControlBar extends Component {
       <div className="control-bar">
         <div className="control-bar__button-tray">
           <div className="control-bar__composer-buttons">
+
             <CopyToClipboard text={this.props.clipboard}>
-              <button className="button button--positive">Copy</button>
+              <div className="icon-button button button--positive control-bar__button">
+                <span className="icon-button__text">Copy</span>
+                <span className="icon-button__icon-wrapper icon-button__icon-wrapper--positive">
+                  <i className="fa fa-files-o icon-button__icon" aria-hidden="true"></i>
+                </span>
+              </div>
             </CopyToClipboard>
-            <button className="control-bar__button button button--negative" onClick={(e) => this.handleClearClick(e)}>Clear</button>
+
+            <div className="icon-button button button--negative control-bar__button" onClick={(e) => this.handleClearClick(e)}>
+              <span className="icon-button__text">Clear</span>
+              <span className="icon-button__icon-wrapper icon-button__icon-wrapper--negative">
+                <i className="fa fa-trash icon-button__icon" aria-hidden="true"></i>
+              </span>
+            </div>
+
           </div>
           <div className="control-bar__tray-buttons">
-            <button className="control-bar__button button button--positive" onClick={(e) => this.openCreateTemplateForm()}>New Template</button>
+            <div className="icon-button button button--positive template-tray-button" onClick={(e) => this.openCreateTemplateForm(e)}>
+              <span className="icon-button__text">New Template</span>
+              <span className="icon-button__icon-wrapper icon-button__icon-wrapper--positive">
+                <i className="fa fa-plus icon-button__icon" aria-hidden="true"></i>
+              </span>
+            </div>
           </div>
         </div>
       </div>
